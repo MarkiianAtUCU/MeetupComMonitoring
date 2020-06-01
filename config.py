a79d07d96174ad46272885d87f4e374f73d2ba84
Markiian Matsiuk <mmatsi@softserveinc.com>
2020-06-01 19:12:14 +0300

    initial commit, MVP

---
+++ config.py
@@ -0,0 +1,10 @@
+HTTP_STREAM_URL = "http://stream.meetup.com/2/rsvps"
+
+AWS_CREDENTIALS = {
+    "aws_access_key_id": "",
+    "aws_secret_access_key": "",
+    "aws_session_token": "",
+}
+
+BOOTSTRAP_SERVERS = ['localhost:9092']
+# BOOTSTRAP_SERVERS = ['localhost:9092', 'localhost:9093', 'localhost:9094']
